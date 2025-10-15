6f806933c27b1372f7166f233053a5402b2d4f42
wchandler2020 <william.d.chandler1@gmail.com>
2025-10-15 00:03:10 -0500

    fix: verify email link

--- src/components/verifyEmail/VerifyEmail.jsx
+++ src/components/verifyEmail/VerifyEmail.jsx
@@ -71,7 +71,7 @@ const VerifyEmail = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen bg-teal-100">
       <div className="bg-white p-8 rounded-lg shadow-md max-w-sm w-full">
         {renderContent()}
       </div>
